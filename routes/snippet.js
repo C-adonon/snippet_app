3b09af672bce1284ba48593910238101a451b4db
Chloé <cadonon.dev@gmail.com>
2023-02-02 11:30:38 +0100

    corrections on post method in snippet.js

--- routes/snippet.js
+++ routes/snippet.js
@@ -17,15 +17,50 @@ const auth = expressjwt({
 //
 // CREATION DE SNIPPETS
 //
-router.post("/", auth, async (req, res, next) => {});
+router.post("/", auth, async (req, res, next) => {
+  const currentUserId = req.auth.id;
+  // Validation zod + Récupère des datas du snippet
+  let snippet;
+  try {
+    snippet = snippetValidator.parse(req.body);
+  } catch (error) {
+    return res.status(400).json({ errors: error.issues });
+  }
+
+  // Vérifie si le snippet est de type Undefined
+  // Si oui, on retourne un tableau vide
+  // Sinon, on continue
+  if (!snippet.tags) {
+    snippet.tags = [];
+  }
+
+  // Crée un snippet à partir des données reçues et envoie dans la BDD
+  const newSnippet = await prisma.snippets.create({
+    data: {
+      title: snippet.title,
+      content: snippet.content,
+      user_id: { connect: { id: currentUserId } },
+      category_id: { connect: { id: snippet.category_id } },
+      tags: {
+        connect: snippet.tags.map((id) => {
+          return { id };
+        }),
+      },
+    },
+  });
+  res.json({ msg: "You have successfully created a snippet!" });
+});
+
 //
 // MODIFICATION DES SNIPPETS
 //
 router.patch("/:id([0-9]+)", auth, async (req, res, next) => {});
+
 //
 // LISTE LES SNIPPETS
 //
 router.get("/", auth, async (req, res, next) => {});
+
 //
 // SUPPRESSION DES SNIPPETS
 //
