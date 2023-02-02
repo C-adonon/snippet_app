bed0ddab52e9ea1570e56d8fc8ff93d0ac29d60d
Chloé <cadonon.dev@gmail.com>
2023-02-02 12:14:24 +0100

    corrections on snippetValidator.js

--- validators/snippetValidator.js
+++ validators/snippetValidator.js
@@ -3,6 +3,8 @@ import { z } from "zod";
 const snippetValidator = z.object({
   title: z.string(),
   content: z.string(),
+  category_id: z.number(),
+  tags: z.optional(z.nullable(z.array(z.number()))),
 });
 
 export { snippetValidator };
